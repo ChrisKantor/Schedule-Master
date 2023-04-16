6e0da0782525d245e306c2420ec75d79d441a4e3
Molina Hoy <molinahoy@usf.edu>
2023-04-16 19:57:40 -0400

    Decorated the home page

--- Schedule Master/client/src/components/home.jsx
+++ Schedule Master/client/src/components/home.jsx
@@ -7,13 +7,18 @@ function Home() {
         <div className="home">
             <Navbar>
             </Navbar>
-            
-            <h1>Home Page of Schedule Master!</h1>
-            
-            {/* brings us to the login page */}
-            <a href="/login">
-                <button>Go To Login Page</button>
-            </a>
+
+
+            <div class="grid-container">
+                <a href="/selection">
+                <div class="courseOption">Course Menu
+                <i className="fa-solid fa-list"></i>
+                </div>
+                </a>
+                <div class="cartOption">Cart
+                <img className = 'cart' src="https://www.citypng.com/public/uploads/small/11640441682j3enmxsqxkzywbh2ojg3uwslf0sm18kpkejhygv7av0ifhqjuf9sk9elom7rk4xg9xykmobez6kfzpf7k2tpkc9tf9sdg6zbhgoq.png"></img>
+                </div>
+            </div>
         </div>
     )
 }
